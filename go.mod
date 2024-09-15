6b2630a3760d227dbbafa79d0b561c0591c3abdb
Andrii Hruzdiev <a.hruzdiev@autodoc.eu>
2024-09-15 18:21:48 +0300

    Big bark

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/And-code/go-repo-puppy
 
 go 1.22.2
+
+require github.com/And-code/go-repo-dog v0.0.0-20240915151626-09727b0dbcaf // indirect
--- puppy.go
+++ puppy.go
@@ -1,9 +1,19 @@
 package go_repo_puppy
 
+import go_repo_dog "github.com/And-code/go-repo-dog"
+
 func Bark() string {
 	return "Woof!"
 }
 
 func Barks() string {
 	return "Woof! Woof! Woof!"
+}
+
+func BigBark() string {
+	return go_repo_dog.WhenGrownUp(Bark())
+}
+
+func BigBarks() string {
+	return go_repo_dog.WhenGrownUp(Barks())
 }
